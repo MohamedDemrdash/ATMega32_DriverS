c92e893149d15974f145fb5fd5738cdb4b30c8c1
Mohamed Demrdash <124625925+MohamedDemrdash@users.noreply.github.com>
2023-02-10 16:53:13 +0200

    Add files via upload

--- MCAL/DIO Driver/DIO_interface.h
+++ MCAL/DIO Driver/DIO_interface.h
@@ -1,20 +1,21 @@
-/******************************************************************************
+/*
+ * DIO_interface.h
  *
- * Module: Dio
- *
- * Layer : MCAL
- *
- * File Name: Dio.c
- *
- * Description: Header file for ATMEGA32A Microcontroller - Dio Driver
- *
- * Author: Mohamed Demrdash
- ******************************************************************************/
+ * Created: 2/6/2023 9:21:31 PM
+ *  Author: Mohamed Demrdash
+ */ 
+
 
 #ifndef DIO_INTERFACE_H_
 #define DIO_INTERFACE_H_
 
 #include "../../utilities/STD_Types.h"
+
+#define  PORT_OUT    0xFF
+#define  PORT_IN     0x00
+
+#define  PORT_HIGH   0xFF
+#define  PORT_LOW    0x00
 
 void DIO_set_pin_dir(uint8_t port,uint8_t pin,uint8_t dir);
 
